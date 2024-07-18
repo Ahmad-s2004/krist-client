6f4c055405a778eda23a71f7a44cc01e476c0fcf
Ahmad-s2004 <shahidahmadamman@gmail.com>
2024-07-18 16:26:24 +0500

    commit

--- src/redux/sliceReducer.js
+++ src/redux/sliceReducer.js
@@ -1,31 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+let initialState = {
+    cart: [],
+    totalAmmount: 0,
+}
+
 const productReducer = createSlice({
     name: 'product',
-    initialState: [],
+    initialState,
     reducers: {
         addToCart: (state, action) => {
             const { product, data } = action.payload;
-            const existingProduct = state.find(item => item._id === product._id);
+            const existingProduct = state.cart.find(item => item._id === product._id);
             
             if (existingProduct) {
                 existingProduct.qnty++; 
             } else {
-                state.push({ ...product, qnty: 1, sizes: data });
+                state.cart.push({ ...product, qnty: 1, sizes: data });
             }
         },
         removeFromCart: (state, action) => {
             const productId = action.payload._id;
-            return state.filter(item => item._id !== productId);
+            return state.cart.filter(item => item._id !== productId);
         },
         removeSingleItem: (state, action) => {
-            const existingProduct = state.find(item => item._id === action.payload.product._id);
+            const existingProduct = state.cart.find(item => item._id === action.payload.product._id);
             if (existingProduct && existingProduct.qnty > 1) {
                 existingProduct.qnty -= 1;
             } 
         },
         clearCart: (state, action)=>{
-            return state = []
+            return state.cart = []
         },
         TotalPrice: (state, action) =>{
             return {
