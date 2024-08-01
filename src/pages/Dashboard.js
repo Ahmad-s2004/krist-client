1a4568e80ffddd70d20892c6a8cab264868deb60
Ahmad-s2004 <shahidahmadamman@gmail.com>
2024-08-01 23:19:53 +0500

    commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,7 +10,7 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 const Dashboard = () => {
   const [value, setValue] = useState('dashboard');
   const[address, setAddress] = useState([])
-  const[user, setUser] = useState({})
+  const[user, setUser] = useState('')
   const [showPassword, setShowPassword] = useState(false)
 
 
