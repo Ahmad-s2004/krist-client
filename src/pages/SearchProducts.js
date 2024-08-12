27706202ae66e7cadf329218fdc70d2aaa6f3819
Ahmad-s2004 <shahidahmadamman@gmail.com>
2024-08-12 23:36:46 +0500

    commit

--- src/pages/SearchProducts.js
+++ src/pages/SearchProducts.js
@@ -7,8 +7,21 @@ const SearchProducts = () => {
 
   const [product, setProduct] = useState([])
   const [loading, setLoading] = useState(true)
+  const [local, setLocal] = useState({
+    category: "",
+    name:""
+  })
   const { search } = useParams()
 
+  if(local.category == 'Kids'){
+    setLocal.name("getKids")
+  }
+  if(local.category == 'Men'){
+    setLocal.name("getMen")
+  }
+  if(local.category == 'Women'){
+    setLocal.name("getWomen")
+  }
   useEffect(() => {
     let fetchData = async () => {
       let res = await fetch(`https://krist-server.vercel.app/product/getProducts?search=${search}`)
@@ -34,13 +47,13 @@ const SearchProducts = () => {
                       <div className="sale my-auto">{Number(x.salePercent)}%</div>
                       <img src={`https://krist-server.vercel.app//${x.gallery[0].img1}`} className="card-img-top primary" alt="Primary" />
                       <img src={`https://krist-server.vercel.app//${x.gallery[0].img2}`} className="card-img-top secondary" alt="Secondary" />
-                      <Link className="button" to={`/getWomen/${x._id}`}>Quick View</Link>
+                      <Link className="button" to={`/${local.name}/${x._id}`}>Quick View</Link>
                     </div>
 
                     <div className="card-body">
                       <h5 className="card-title d-none d-lg-block h6">{x.title.slice(0, 40)}...</h5>
                       <h5 className="card-title d-block d-lg-none" style={{ fontSize: "0.8rem" }}>{x.title.slice(0, 40)}...</h5>
-                      <p className="card-text d-none d-lg-block">Rs. {Math.floor(x.price - x.price / x.salePercent)}</p>
+                      <p className="card-text d-none d-lg-block">Rs. {Math.floor(x.price - x.price / x.salePercent)}{setLocal.category(x.category)}</p>
                       <p className="card-text d-block d-lg-none" style={{ fontSize: "0.8rem" }}>Rs. {Math.floor(x.price - x.price / x.salePercent)}</p>
                     </div>
                   </div>
